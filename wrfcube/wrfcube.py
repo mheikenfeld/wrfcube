Fixed calculation of surface precipitation (removed diving by 24 that was an artefact of the previously used time axis)

wrfcube/wrfcube.py
@@ -383,7 +383,7 @@ def calculate_wrf_surface_precipitation(filenames,constraint=None,add_coordinate
     RAINNC= loadwrfcube(filenames, 'RAINNC',add_coordinates=add_coordinates)
     #caclulate timestep in hours
     time_coord=RAINNC.coord('time')
-    dt=(time_coord.units.num2date(time_coord.points[1])-time_coord.units.num2date(time_coord.points[0])).total_seconds()/24./3600.
+    dt=(time_coord.units.num2date(time_coord.points[1])-time_coord.units.num2date(time_coord.points[0])).total_seconds()/3600.
     #divide difference in precip between timesteps (in mm/h) by timestep (in h):
     rainnc_inst=np.concatenate((RAINNC.core_data()[[1],:,:]-RAINNC.core_data()[[0],:,:],RAINNC.core_data()[1:,:,:]-RAINNC.core_data()[0:-1:,:,:]),axis=0)/dt
     RAINNC_inst=RAINNC