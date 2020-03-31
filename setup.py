fb6e9b11e3b6be14be63cecca7d83a2c592c9ed3
Max Heikenfeld <maxheikenfeld@web.de>
2020-03-31 18:48:54 +0200

    Update setup.py: License, version based on github tag

--- setup.py
+++ setup.py
@@ -1,12 +1,13 @@
 from setuptools import setup
 
 setup(name='wrfcube',
-      version='1.0.1',
+      use_scm_version=True,
+      setup_requires=['setuptools_scm'],
       description='Load WRF output into iris cubes',
       url='http://github.com/mheikenfeld/wrfcube',
       author='Max Heikenfeld',
       author_email='maxheikenfeld@web.de',
-      license='GNU',
+      license='BSD-3-Clause',
       packages=['wrfcube'],
       install_requires=[],
       zip_safe=False)
